8c7e4ce4df0983c5afcf8a0ebe67ce72ebfe10f0
thomasferew <58610700+thomasferew@users.noreply.github.com>
2020-02-06 13:14:38 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+if (true && true)
+    
+    game.splash("true and true is ALWAYS result in true")
+
+
+
+if (true && false) {
+
+    game.splash(" true and false, the result is ALWAYS false")
+
+}
+
+
+if (true || true)
+
+    game.splash("if both things are true then the result is true")
+
+
+
+if (true ||false)
+    game.splash("if one thing is true, the the result is TRUE")
